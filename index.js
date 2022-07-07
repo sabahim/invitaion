e813572079bf97e3708fb0b9d65725b6dc8e6f1f
sabahim <108833503+sabahim@users.noreply.github.com>
2022-07-07 12:11:41 +0430

    Add files via upload

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-var context = new AudioContext();
-context.resume();
+const presentBtn = document.querySelector(".present-btn");
+const invitation = document.querySelector(".container");
+const invitationImage = document.querySelector(".invitation-png");
+const particles = document.querySelector("#confetti");
+const audio = document.querySelector(".sound");
+const background = document.querySelector(".background");
+presentBtn.addEventListener("click", showInvitation);
+
+function showInvitation(e) {
+    presentBtn.style.display = "none";
+
+    invitation.style.display = "flex";
+    invitation.style.opacity = 1;
+    particles.style.display = "block";
+    invitation.style.transform = "translateY(0)";
+    audio.currentTime = 0; //rewind to the start of the sound
+    audio.play();
+}
